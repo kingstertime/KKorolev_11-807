609bd20384d85921b3b44dee431205780d390aef
kingstertime <kingstertime@gmail.com>
2018-09-20 21:43:27 +0300

    homework4.java added

---
+++ Lesson05/src/homework4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class homework4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println("Enter symbol-key:");
+        String k = sc.next();
+        if (k.equals("u") || k.equals("U")) {
+            s = s.toUpperCase();
+        }
+        if (k.equals("l") || k.equals("L")) {
+            s = s.toLowerCase();
+        }
+        System.out.println(s);
+    }
+}
