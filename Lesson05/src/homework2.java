9a971b097475d134a94468603dcb610e33124c0e
Kirill <43263943+kingstertime@users.noreply.github.com>
2018-09-25 12:35:21 +0300

    Update homework2.java

--- Lesson05/src/homework2.java
+++ Lesson05/src/homework2.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class homework2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine(); //bagfix
+        String s = sc.nextLine();
         int lenght = s.length();
         char[] a = s.toCharArray();
         for (int i = 0; i < lenght; i++) {
