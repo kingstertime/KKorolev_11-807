b5e49d818b0ad5f19445d64c9231375a12eead40
Кирилл Королёв <kingstertime@gmail.com>
2020-04-22 11:23:44 +0300

    add 11

---
+++ 4nd semester/11/LoggingAspect.java
@@ -0,0 +1,30 @@
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+ 
+@Aspect
+@Component
+ 
+public class LoggingAspect {
+ 
+    @Before("execution(* ru.springuser.repositories.CrudRepository.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+ 
+        System.out.println("!log");
+        System.out.println("watch " + joinPoint.getSignature().getName());
+    }
+ 
+    @AfterThrowing (pointcut = "execution(* *(..))", throwing = "ex")
+    public void logAllThrowingMethods(Exception ex) throws Throwable {
+        System.err.println("Exception throwed : "+ ex);
+    }
+ 
+    @Before("@annotation(ru.springuser.annotation.MyLog)")
+    public void log(JoinPoint joinPoint) throws Throwable {
+        System.out.println("watch " + joinPoint.getSignature().getName());
+    }
+}
